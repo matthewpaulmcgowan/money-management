b7c30f96e2988e597f826ad10bf0820be4bc72bb
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-07-16 19:48:24 -0500

    mocked up item state.

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -7,6 +7,14 @@ angular
               templateUrl: '/templates/home/home.html',
               controller: "HomeController as home"
             })
+            .state('item', {
+              url: '/item',
+              templactUrl: '/templates/item/show.index.html',
+              controller: "ItemController as item",
+              resolve{
+                //add in service http call to get list of items
+              }
+            })
 
         $urlRouterProvider.otherwise("/home");
 
