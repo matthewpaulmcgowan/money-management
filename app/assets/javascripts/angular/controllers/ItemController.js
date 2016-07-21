b10e13d9881b47254caefb93ed21c5fad6f6cfad
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-07-21 14:04:37 -0500

    injected ItemService into ItemService.

--- app/assets/javascripts/angular/controllers/ItemController.js
+++ app/assets/javascripts/angular/controllers/ItemController.js
@@ -1,19 +1,21 @@
-function ItemController(item){
+function ItemController(item, ItemService){
   var ctrl = this;
   ctrl.data=item.data;
 
-  this.editItem(){
+  this.editItem = function(id){
     var params = {
       name: ctrl.name
     }
 
     ItemService
-         .updateItem(params)
+         .updateItem(params, id)
          .then(function(response){
            $state.reload();
          })
   }
 }
+
+ItemController.$inject = ['item', 'ItemService'];
 
 angular
       .module("app")
