ccec527b4773619ce2fae1cbb3adbb61bf14f278
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-07-21 23:16:41 -0500

    added signup service call.

--- app/assets/javascripts/angular/services/UserService.js
+++ app/assets/javascripts/angular/services/UserService.js
@@ -2,6 +2,10 @@ function UserService($http){
   this.userLogin = function (params) {
         return $http.post('http://localhost:3000/login', params);
     }
+
+  this.userSignup = function (params) {
+        return $http.post("http://localhost:3000/signup", params)
+  }
 }
 
 angular
