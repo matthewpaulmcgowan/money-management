9e8aa1e83f47a03a16d6ab0e8c4d1493eea33cb1
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-07-21 22:23:08 -0500

    angular login controller setup.

--- app/assets/javascripts/angular/controllers/HomeLoginController.js
+++ app/assets/javascripts/angular/controllers/HomeLoginController.js
@@ -1,6 +1,22 @@
-function HomeLoginController (){
+function HomeLoginController (UserService, $state){
+  var ctrl = this;
 
+  ctrl.login = function(){
+    var params = {
+      username: this.username,
+      password: this.password
+    }
+
+    UserService
+         .userLogin(params)
+         .then(function(response){
+            debugger;
+           //$state.reload();
+         })
+  }
 }
+
+HomeLoginController.$inject = ['UserService', '$state'];
 
 angular
       .module('app')
