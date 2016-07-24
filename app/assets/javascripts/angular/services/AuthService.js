4f6507bacc846183d7f40eb771d8b00d70a48f82
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-07-24 16:39:08 -0500

    added loggedIncheck call to rails.

--- app/assets/javascripts/angular/services/AuthService.js
+++ app/assets/javascripts/angular/services/AuthService.js
@@ -1,7 +1,8 @@
 function AuthService(){
 
-  this.loggedInCheck = function(){
-      debugger;
+  this.loggedInCheck = function($http){
+          return $http.get('http://localhost:3000/users/loggedIn');
+      }
   }
 
 }
