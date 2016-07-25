b6892c6bba544572c3f7d714f69df9b542ebbb6a
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-07-24 19:26:49 -0500

    call to check cookie in items controller is working.

--- app/assets/javascripts/angular/controllers/ItemsController.js
+++ app/assets/javascripts/angular/controllers/ItemsController.js
@@ -1,14 +1,16 @@
 function ItemsController(items, CookiesService, $window, $state){
   var ctrl = this;
 
-  this.checkCookie = function(){
-    debugger;
-    if(!CookiesService.getCookie){
+  ctrl.checkCookie = function(){
+    //debugger;
+    if(!CookiesService.getCookie()){
       $state.go("home")
       $window.alert("Not Logged In, Please Sign In or Create a New Profile")
     }
   }
+
   ctrl.data = items.data;
+  ctrl.checkCookie();
 }
 
 ItemsController.$inject = ['items', 'CookiesService', '$window', '$state'];
