4ee850e9ea957685561262c78994486b24e14e4f
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-07-25 14:13:24 -0500

    params now only includes item categories that are not empty, in edit item controller.

--- app/assets/javascripts/angular/controllers/ItemController.js
+++ app/assets/javascripts/angular/controllers/ItemController.js
@@ -1,12 +1,24 @@
-function ItemController(item, ItemService, $state, CookiesService){
+function ItemController(item, ItemService, $state, CookiesService, EditCheckService){
   var ctrl = this;
   ctrl.data=item.data;
   CookiesService.checkCookie();
 
   ctrl.editItem = function(id){
-    var params = {
-      name: ctrl.name
+    var params = {};
+
+    if(EditCheckService.checkIfChanged(form.amount.classList)){
+      params['amount'] = ctrl.amount;
     }
+
+    if(EditCheckService.checkIfChanged(form.name.classList)){
+      params['name'] = ctrl.name;
+    }
+
+    if(EditCheckService.checkIfChanged(form.category.classList)){
+      params['category'] = ctrl.category;
+    }
+
+    debugger;
 
     ItemService
          .updateItem(params, id)
@@ -17,7 +29,7 @@ function ItemController(item, ItemService, $state, CookiesService){
 
 }
 
-ItemController.$inject = ['item', 'ItemService', '$state', 'CookiesService'];
+ItemController.$inject = ['item', 'ItemService', '$state', 'CookiesService', 'EditCheckService'];
 
 angular
       .module("app")
