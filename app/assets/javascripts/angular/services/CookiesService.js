694b599f36276bd4c9e054e6336edb9fb45ad837
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-07-24 21:55:22 -0500

    added checkCookie function to see if a cookie is stored.

--- app/assets/javascripts/angular/services/CookiesService.js
+++ app/assets/javascripts/angular/services/CookiesService.js
@@ -1,4 +1,4 @@
-function CookiesService($cookies){
+function CookiesService($cookies, $state, $window){
 
   this.setCookie = function(id){
     $cookies.put("userId", id);
@@ -6,6 +6,13 @@ function CookiesService($cookies){
 
   this.getCookie = function(){
     return $cookies.get('userId');
+  }
+
+  this.checkCookie = function(){
+    if(!this.getCookie()){
+      $state.go("home")
+      $window.alert("Not Logged In, Please Sign In or Create a New Profile")
+    }
   }
 }
 
