780feedc279c71cf5397ff8308a6d2b31ccaabf6
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-07-24 19:15:44 -0500

    added call to cookies service to check if cookie has beeen set.

--- app/assets/javascripts/angular/controllers/ItemsController.js
+++ app/assets/javascripts/angular/controllers/ItemsController.js
@@ -1,9 +1,17 @@
-function ItemsController(items){
+function ItemsController(items, CookiesService, $window, $state){
   var ctrl = this;
+
+  this.checkCookie = function(){
+    debugger;
+    if(!CookiesService.getCookie){
+      $state.go("home")
+      $window.alert("Not Logged In, Please Sign In or Create a New Profile")
+    }
+  }
   ctrl.data = items.data;
 }
 
-ItemsController.$inject = ['items'];
+ItemsController.$inject = ['items', 'CookiesService', '$window', '$state'];
 
 angular
         .module("app")
