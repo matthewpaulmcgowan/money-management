0f2eb5b9b8ff23ef2f9b22b5f8cae114c06cf1e7
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-08-01 17:34:20 -0500

    changed index controller to itemsController item creation and ctrl is now available in the then promise of the createItem function call.

--- app/assets/javascripts/angular/controllers/ItemsController.js
+++ app/assets/javascripts/angular/controllers/ItemsController.js
@@ -1,4 +1,4 @@
-function ItemsController(items, CookiesService, $window, $state, $filter){
+function ItemsController(items, CookiesService, $window, $state, $filter, ItemService){
   var ctrl = this;
 
   CookiesService.checkCookie();
@@ -9,10 +9,35 @@ function ItemsController(items, CookiesService, $window, $state, $filter){
 
   ctrl.data = items.data;
 
+  ctrl.createItem = function(){
+    var userId = CookiesService.getCookie();
+debugger;
+    var params = {
+      name: ctrl.name,
+      category: ctrl.category,
+      amount: ctrl.amount,
+      userId: userId
+    }
+
+    ItemService
+      .createItem(params)
+      .then(function (response) {
+        ctrl.addResponse(response);
+          debugger;
+        //response {name: ""}
+        // ctrl.items.data.push(response)
+      })
+      debugger;
+  }
+
+  ctrl.addResponse = function (response){
+    debugger;
+  }
+
   ctrl.filteredList = $filter('largeAmount')(ctrl.data);
 }
 
-ItemsController.$inject = ['items', 'CookiesService', '$window', '$state', '$filter'];
+ItemsController.$inject = ['items', 'CookiesService', '$window', '$state', '$filter', "ItemService"];
 
 angular
         .module("app")
