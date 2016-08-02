27abf9d9406667c21e1df8a0b70f19f5717d1bbe
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-08-02 03:42:39 -0500

    working on accessing ItemsController, not yet working.

--- app/assets/javascripts/angular/components/item.js
+++ app/assets/javascripts/angular/components/item.js
@@ -1,10 +1,27 @@
 var Item = {
   templateUrl: "/templates/items/item.html",
   bindings: {
-    id: '=', name: '=', category: '=', amount: '='
+    id: '=', name: '=', category: '=', amount: '=', index: "=", deleteItem: "&"
   },
   controllerAs: "item",
+  controller:
+    function (ItemService, $scope) {
+      var ctrl = this;
+
+      ctrl.delete = function (identifier) {
+        debugger;
+        ctrl.deleteItem({id: identifier});
+        debugger;
+        //ItemService
+        //  .deleteItem(id)
+        //  .then(function (response){
+
+        //  })
+        };
+      }
 }
+
+//Item.$inject = ['ItemsController'];
 
 angular
       .module("app")
