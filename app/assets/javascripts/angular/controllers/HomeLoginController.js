65ecda5d5e4ade3505e02bd25774897900c0af41
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-08-04 17:00:28 -0500

    ng-token-auth is configured and sucessfully making request to rails, rails currently giving 401.

--- app/assets/javascripts/angular/controllers/HomeLoginController.js
+++ app/assets/javascripts/angular/controllers/HomeLoginController.js
@@ -6,34 +6,36 @@ function HomeLoginController (UserService, $state, $window, CookiesService, $aut
   }
 
   ctrl.login = function(){
+
     var params = {
       email: this.email,
       password: this.password
     }
+
     debugger;
-    $auth.submitLogin(params)
-        .then(function(resp) {
-          debugger;
-          // handle success response
-        })
-        .catch(function(resp) {
-          debugger
-          // handle error response
-        });
+  //  $auth.submitLogin(params)
+  //      .then(function(resp) {
+  //        debugger;
+  //        // handle success response
+  //      })
+  //      .catch(function(resp) {
+  //        debugger
+  //        // handle error response
+  //      });
 
 
-    //UserService
-    //  .userLogin(params)
-    //  .then(function(response){
-    //    if(response.data.id){
-    //      CookiesService.setCookie(response.data.id);
-    //      $state.go("items");
-    //    }
-    //    else{
-    //      $state.reload();
-    //      $window.alert("User Login Failed, Please Try Again")
-    //    }
-    //  })
+    UserService
+      .userLogin(params)
+      .then(function(response){
+        if(response.data.id){
+          CookiesService.setCookie(response.data.id);
+          $state.go("items");
+        }
+        else{
+          $state.reload();
+          $window.alert("User Login Failed, Please Try Again")
+        }
+      })
   }
 }
 
--- app/assets/javascripts/angular/services/UserService.js
+++ app/assets/javascripts/angular/services/UserService.js
@@ -1,6 +1,15 @@
-function UserService($http){
+function UserService($http, $auth){
   this.userLogin = function (params) {
-    return $http.post('http://localhost:3000/login', params);
+  return $auth.submitLogin(params)
+        //.then(function(resp) {
+        //  debugger;
+        //  // handle success response
+        //})
+        //.catch(function(resp) {
+        //  debugger
+        //  // handle error response
+        //});
+    //return $http.post('http://localhost:3000/login', params);
   }
 
   this.userSignup = function (params) {
