1e34857df1a2f9f841fa0fac2d2fbfbb0854699a
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-08-08 14:52:35 -0500

    amount total per category is sucessfully being calculated, formatted, and sent to angular.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,13 +3,11 @@ class CategoriesController < ApplicationController
   def index
     @user = current_user
     @amount_total = {}
-    @user.categories.each do |category|
-      binding.pry
+    @user.categories.uniq.each do |category|
       @total = category.get_amount_total(@user.id)
-      binding.pry
-      @amount_total[:'#{category.name}'] = @total
+      @amount_total[category.name] = @total
     end
-    render json: {}
+    render json: @amount_total 
   end
 
 end
