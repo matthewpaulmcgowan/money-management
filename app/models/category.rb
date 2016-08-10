7b342894b53067adfa40759a95ef94416f09bc11
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-08-10 16:01:35 -0500

    made use of category association.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,8 @@ class Category < ActiveRecord::Base
   has_many :items
 
   def get_amount_total(user_id)
-    @items = Item.where(category_id: self.id, user_id: user_id)
-    @items.inject(0) do |sum, item|
+    items = self.items.where(user_id: user_id)
+    items.inject(0) do |sum, item|
       sum + item.amount
     end
   end
